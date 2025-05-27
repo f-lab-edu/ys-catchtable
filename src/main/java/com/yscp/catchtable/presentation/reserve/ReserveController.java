60158c29dcec81fcd9f4f26b5b83e26654b2a363
yushin <kkad45@naver.com>
2025-05-27 22:58:12 +0900

    feature/예약하기 불필요한 클래스 제거

--- src/main/java/com/yscp/catchtable/presentation/reserve/ReserveController.java
+++ src/main/java/com/yscp/catchtable/presentation/reserve/ReserveController.java
@@ -3,11 +3,9 @@ package com.yscp.catchtable.presentation.reserve;
 import com.yscp.catchtable.application.reserve.ReserveService;
 import com.yscp.catchtable.application.reserve.ReservesInDayDto;
 import com.yscp.catchtable.application.reserve.UserReserveAggregateService;
-import com.yscp.catchtable.application.reserve.UserReserveService;
 import com.yscp.catchtable.presentation.reserve.dto.StoreReserveRequestDto;
 import com.yscp.catchtable.presentation.reserve.dto.response.ReserveInDayResponseDtos;
 import lombok.RequiredArgsConstructor;
-import org.redisson.api.RedissonClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +15,12 @@ import java.time.LocalDate;
 @RestController
 public class ReserveController {
     private final UserReserveAggregateService userReserveAggregateService;
-    private final UserReserveService userReserveService;
     private final ReserveService reserveService;
-    private final RedissonClient redissonClient;
 
     @GetMapping("/store/reserves/{storeIdx}")
     public ResponseEntity<ReserveInDayResponseDtos> getReservesInDay(@PathVariable Long storeIdx,
                                                                      @RequestParam LocalDate date) {
-
         ReservesInDayDto reservesInDay = reserveService.getReservesInDay(storeIdx, date);
-
         return ResponseEntity.ok(ReserveInDayResponseDtos.from(reservesInDay));
     }
 
